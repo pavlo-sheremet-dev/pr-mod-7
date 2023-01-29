c46686b0fa57512e6aee99ef2e78b1d9ea273d59
pavlo-sheremet-dev <goit.pavel.sheremet@gmail.com>
2023-01-29 12:57:33 +0200

    remove

--- redux/selectors.js
+++
@@ -1,5 +0,0 @@
-export const selectComments = (state) => state.comments.items;
-
-export const selectIsLoading = (state) => state.comments.isLoading;
-
-export const selectError = (state) => state.comments.error;
